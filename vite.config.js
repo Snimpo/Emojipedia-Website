ff600e7b62b8460fcad31f041702a8cbf9a809bf
Silvio <nirvash533@gmail.com>
2024-10-30 12:17:57 -0500

    added a new line on the vite.conifg so it can read js and jsx files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/Emojipedia-Website/',  // Ensure this matches your GitHub Pages URL path
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx'],  // Add .jsx to the list of extensions
+  },
 });
